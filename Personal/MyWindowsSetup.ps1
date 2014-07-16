bc436de6eb29da7dce96e17e5661626b48cd45d2
Adrian Padilla Vega <adrian@adrianpadilla.net>
2014-07-16 12:02:10 -0700

    Started my windows setup script with just some SW and tools

---
+++ Personal/MyWindowsSetup.ps1
@@ -0,0 +1,27 @@
+﻿#Install Chocolately http://chocolatey.org to enable installations
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Software
+cinst GoogleChrome
+cinst adobereader
+cinst spotify
+
+#Installing dev tools
+cinst notepadplusplus
+cinst vim
+cinst linqpad4
+cinst winmerge
+cinst terminals
+cinst python
+cinst XmlNotepad
+cinst GitHub
+cinst Atom
+cinst wireshark
+cinst sysinternals
+cinst fiddler
+
+
+#installing misc tools
+cinst 7zip.install
+cinst winrar
+cinst keepass
