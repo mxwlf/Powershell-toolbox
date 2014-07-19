2b143fa9c75fdad378c088202b8b86d2497df1f6
Adrian Padilla Vega <adrian@adrianpadilla.net>
2014-07-19 14:03:14 -0700

    Add path and personal setupo under test

--- Personal/MyWindowsSetup.ps1
+++ Personal/MyWindowsSetup.ps1
@@ -6,6 +6,20 @@ cinst GoogleChrome
 cinst adobereader
 cinst spotify
 
+#Visual Studio. replace product key.
+<#available features
+Blend
+LightSwitch
+VC_MFC_Libraries
+OfficeDeveloperTools
+SQL
+WebTools
+Win8SDK
+SilverLight_Developer_Kit
+WindowsPhone80
+#>
+#cinst VisualStudio2013Ultimate -InstallArguments "/Features:'WebTools Win8SDK' /ProductKey:AB1CD-EF2GH-IJ3KL-MN4OP-QR5ST"
+
 #Installing dev tools
 cinst notepadplusplus
 cinst vim
@@ -24,4 +38,5 @@ cinst fiddler
 #installing misc tools
 cinst 7zip.install
 cinst winrar
+cinst vmwareplayer
 cinst keepass
---
+++ Under test/Add-PathEnvEntry.ps1
@@ -0,0 +1,35 @@
+﻿Param(
+    [string]$newPath
+)
+
+#Validate path
+if(-not $newPath)
+{
+    throw "Must specify a path to add"
+}
+
+if( -not (Test-Path $newPath))
+{
+    throw "Invalid path. Must be a correct path that maps to an existing location"
+}
+
+#Constants
+$registryEntry = 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment'
+
+#Normalize the path to be drive rooted.
+$newPath = [System.IO.Path]::GetFullPath($newPath)
+
+#Gets the current path.
+$pathBefore = (Get-ItemProperty -Path $registryEntry -Name PATH).Path
+
+#Paths may have a trailing semicolon.
+$separator = $null
+if( -not $pathBefore.EndsWith(';'))
+{
+    $separator = ';'
+}
+
+#Building and updating the final string.
+$pathUpdated = $pathBefore + $separator + $newPath
+
+Set-ItemProperty -Path $registryEntry -Name PATH –Value $pathUpdate
